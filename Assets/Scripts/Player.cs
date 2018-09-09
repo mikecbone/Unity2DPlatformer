1c60a41eb1c95dadf39740e3f79fcedcae7a4547
Mike <mikecbone@gmail.com>
2018-09-09 13:37:12 +0100

    332
    Moving player horizontally

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D rigidBody2D;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+	}
+
+    private void Run() {
+        float controlThrow = Input.GetAxisRaw("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidBody2D.velocity.y);
+        rigidBody2D.velocity = playerVelocity;
+    }
+}

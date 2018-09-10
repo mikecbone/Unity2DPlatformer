283bbc643406558b59e4f72bd9cde367b6c2d58a
Mike <mikecbone@gmail.com>
2018-09-10 14:34:10 +0100

    Audio SFX for coins

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinPickup : MonoBehaviour {
+    [SerializeField] AudioClip coinPickupSFX;
+    private SFXplayer sfxplayer;
+
+    private void Start() {
+        sfxplayer = FindObjectOfType<SFXplayer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
+        //AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        sfxplayer.PlaySFX(coinPickupSFX);
         Destroy(this.gameObject);
     }
 }

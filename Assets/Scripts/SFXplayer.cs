640dbd58a2c7c9ff5eab697e670bc12c018b89b1
Mike <mikecbone@gmail.com>
2018-09-10 14:34:10 +0100

    353
    Audio SFX for coins

---
+++ Assets/Scripts/SFXplayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXplayer : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	public void PlaySFX(AudioClip sfx) {
+        audioSource.PlayOneShot(sfx);
+    }
+}

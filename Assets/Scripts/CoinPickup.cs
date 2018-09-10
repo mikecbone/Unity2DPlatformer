b8de165d21f423403da16088c2f66f9d7049df14
Mike <mikecbone@gmail.com>
2018-09-10 16:07:25 +0100

    354b
    Persistent coins and coin scoring images

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int coinScore = 1;
+
     private SFXplayer sfxplayer;
+    private bool addedToScore = false;
 
     private void Start() {
         sfxplayer = FindObjectOfType<SFXplayer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
+        if (addedToScore) {
+            return;
+        }
+        addedToScore = true;
         sfxplayer.PlaySFX(coinPickupSFX);
+        FindObjectOfType<GameSession>().AddToScore(coinScore);
         Destroy(this.gameObject);
     }
 }

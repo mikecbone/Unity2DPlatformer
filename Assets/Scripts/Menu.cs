802081943633abc71b3bc799cd26221b211aca9d
Mike <mikecbone@gmail.com>
2018-09-11 12:52:31 +0100

    Polish3
    Fixed menu music not playing on web version, improved maps, added wall jump areas, added 50 coin total to collect, finish level 4 etc

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,10 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
-    private void Start() {
-        GameSession.instance.PlayMenuMusic();
-    }
-
     public void StartFirstLevel() {
         GameSession.instance.PlayLevelMusic();
         SceneManager.LoadScene(1);
